58006e654a9ecf66ca4898ee1239344b7109625f
Dawid Ruth <dawidolinp1@gmail.com>
2025-01-16 18:07:15 +0100

    add db context

---
+++ GymApp/Data/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using GymApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GymApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Calendar> Calendars { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //foreign key in Reservation table
+            modelBuilder.Entity<Reservation>()
+                .HasOne<Calendar>()
+                .WithMany()
+                .HasForeignKey(r => r.ClassId)
+                .OnDelete(DeleteBehavior.Cascade);  
+        }
+    }
+}
