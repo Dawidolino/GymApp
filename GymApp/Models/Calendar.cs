60eb1cba8b917bec22702daa296bfbe8fe920571
Dawid Ruth <dawidolinp1@gmail.com>
2025-01-16 18:06:49 +0100

    add models

---
+++ GymApp/Models/Calendar.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace GymApp.Models
+{
+    public class Calendar
+    {
+        public int Id {get; set;}
+        
+        [Required]
+        [MaxLength(100)]
+        public string Title { get; set;}
+
+        [Required]
+        public string? Instructor { get; set;}
+        [Required]
+        public DateTime Date { get; set;}
+        [Required]
+        public TimeSpan StartTime { get; set;}
+        [Required]
+        public TimeSpan EndTime { get; set;}
+        [Required]
+        [Range(1,100)]
+        public int Capacity { get; set;}
+        [Required]
+        [Range(0,120)]
+        public int AvailableSlots { get; set;}
+
+    }
+}
---
+++ GymApp/Models/Reservation.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GymApp.Models
+{
+    public class Reservation
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int ClassId { get; set; }
+
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string UserEmail { get; set; }
+
+        public DateTime ReservationDate { get; set; } = DateTime.Now;
+
+    }
+}
